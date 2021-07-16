16efb9e5a6961bb3a78fcf6246212af4db7077bb
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-16 15:44:09 -0500

    [UPDATE] header

---
+++ header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<?php include("config-ini.php"); ?>
+<title><?php echo $titulo; ?></title>
+
+  <!-- Rodrigo Zarate A-->
+  <!-- Agosto 2020 -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  
+  <link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
+ 
+<script src="utilidades/script.js" type="text/javascript"></script>
+<!--
+<script src="utilidades/gestion.js" type="text/javascript"></script>
+-->
+
+</head>
+<body id="page-top">
+
+  <!-- Page container -->
+  <div id="container">
+
+
